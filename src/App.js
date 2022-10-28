94a1ac18749d9be6a94456acacca40989f6b035d
Wali Hassan <i202429@nu.edu.pk>
2022-10-28 18:41:11 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 
 import './App.css';
 import Button from '@material-ui/core/Button';
-import SaveIcon from '@material-ui/icons/Save';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { ButtonGroup, FormControlLabel } from '@material-ui/core';
-import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles, ThemeProvider, createTheme } from '@material-ui/core/styles'; //import for styles and themes
 import React, { useEffect, useState , Component} from 'react';
 import { orange, purple } from '@material-ui/core/colors';
 import 'fontsource-roboto'; //importing fonts, this is a google font, so we need to import it from google
-import Typography from '@material-ui/core/Typography'; //this is required for the better moving and typography of the page
-import Container from '@material-ui/core/Container'; //containers for MUI
-import Paper from '@material-ui/core/Paper'; //helps in seeing the grid for MUI a bit better
-import Grid from '@material-ui/core/Grid';
-import AppBar from '@material-ui/core/AppBar'; //this is the app bar, which is the bar at the top of the page
-import Toolbar from '@material-ui/core/Toolbar'; //this for the tool bar
-import IconButton from '@material-ui/core/IconButton'; //this for a button with an icon on it
-import MenuIcon from '@material-ui/icons/Menu'; //and this is the actual icon that we need
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+
  //and remember, this entire thing is a const, so its getting called within a function, so remember the ending normal bracket
 
 
@@ -60,7 +43,23 @@ const useStyles = makeStyles({ //this makes it so that you can simply make calls
   }
 })
  
+const ButtonStyled=()=>{ //then you can make whatever you want to use it for a function, and then call it within the return statement
+  const classes = useStyles()
+  return <Button className={classes.root}>Delete</Button> //like so, i wanted a button styled, so i used it to call a button, just remember to add in a classname call using classes.root
+}
+
+ const createData=(bloodbank, patientNO, TIME, bloodgroup,date)=> {
+  return { bloodbank, patientNO, TIME, bloodgroup,date };
+ }
  
+const deleteRow = (e) => {
+  var rows = [
+  createData("RBC bloodbank", 5, "21:00","O+","03/01/22"),
+  createData("DONERS bloodbank", 8, "21:00","O+","04/05/22"),                                                                                                                 
+  createData("LIFE SAVER", 9, "21:00","O+","06/08/22"),
+  createData("ONE DROP", 15, "21:00","O+","15/11/22"),
+  createData("GOD'S PLAN", 7, "21:00","O+","30/01/23")
+ ];
 
 
 class App extends React.Component() {
@@ -76,99 +75,59 @@ class App extends React.Component() {
     safe: []
     }
   }
-  
- 
- ButtonStyled=()=>{ //then you can make whatever you want to use it for a function, and then call it within the return statement
-  const classes = useStyles()
-  return <Button className={classes.root}>Delete</Button> //like so, i wanted a button styled, so i used it to call a button, just remember to add in a classname call using classes.root
+  render()
+    {
+      return ( 
+        <ThemeProvider theme={theme}> {/*this is how you use the theme provider, and then you can call the theme within the theme provider*/} {/*btw is you're wondering what's up with the curly braces, then in the return component of this, you can only comment like this, otherwise it'll show up in the website */}  
+        <Container maxWidth='lg' >
+        <div className="App">
+          <header className="App-header">
+            <AppBar color="secondary"> {/*add in the app bar at the top, and then you can add in the color of the app bar*/}
+              <Toolbar color="primary"> {/*add in the toolbar, and then you can add in the color of the toolbar*/}
+                <IconButton color="primary"> {/*add in the icon button, and then you can add in the color of the icon button*/}
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6"> {/* this specifies what font size we want out menu text to be, an h6 is nice for this one*/}
+                  APPOINTMENTS
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Typography variant="h10" component="div" >
+            <TableContainer>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>BLOODBANK</TableCell>
+                <TableCell align="right">PATIENT NO</TableCell>
+                <TableCell align="right">TIME</TableCell>
+                <TableCell align="right">BLOOD GROUP</TableCell> 
+                <TableCell align="right">DATE</TableCell>
+                <TableCell align="right"> OPTIONS</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.number}>
+                  <TableCell>{row.bloodbank}</TableCell>
+                  <TableCell align="right">{row.patientNO}</TableCell>
+                  <TableCell align="right">{row.TIME}</TableCell>
+                  <TableCell align="right">{row.bloodgroup}</TableCell>
+                  <TableCell align="right">{row.date}</TableCell>
+                  <ButtonStyled align="right" style={{padding: 15}} onClick={deleteRow(row.number)}/>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      
+        </Typography>
+          </header>
+        </div>
+        </Container>
+        </ThemeProvider>
+      );
+    }
+  }
 }
-
- const createData=(bloodbank, patientNO, TIME, bloodgroup,date)=> {
-  return { bloodbank, patientNO, TIME, bloodgroup,date };
- }
- 
-const deleteRow(row)
- {
-
- }
-  var rows = [
-  createData("RBC bloodbank", 5, "21:00","O+","03/01/22"),
-  createData("DONERS bloodbank", 8, "21:00","O+","04/05/22"),                                                                                                                 
-  createData("LIFE SAVER", 9, "21:00","O+","06/08/22"),
-  createData("ONE DROP", 15, "21:00","O+","15/11/22"),
-  createData("GOD'S PLAN", 7, "21:00","O+","30/01/23")
- ];
-render()
-{
-  return ( 
-    <ThemeProvider theme={theme}> {/*this is how you use the theme provider, and then you can call the theme within the theme provider*/} {/*btw is you're wondering what's up with the curly braces, then in the return component of this, you can only comment like this, otherwise it'll show up in the website */}  
-    <Container maxWidth='lg' >
-    <div className="App">
-      <header className="App-header">
-        <AppBar color="secondary"> {/*add in the app bar at the top, and then you can add in the color of the app bar*/}
-          <Toolbar color="primary"> {/*add in the toolbar, and then you can add in the color of the toolbar*/}
-            <IconButton color="primary"> {/*add in the icon button, and then you can add in the color of the icon button*/}
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6"> {/* this specifies what font size we want out menu text to be, an h6 is nice for this one*/}
-              APPOINTMENTS
-            </Typography>
-           {/*} <Button align="right">UPDATE</Button> */}
-          </Toolbar>
-        </AppBar>
-       {/* <Grid container spacing={4} justify="center"> {/*this element is the grid container, which specifies grid objects within it, and adds in styling for everything underneath it, pretty useful*/}
-       {/*</header> <Grid item > {/*this is the grid item, which is the actual grid object, and it can be used to specify the size of the grid object, and other things*/}
-         {/* <Paper style={{height:75, width:'100%',}}></Paper>
-        </Grid> 
-        <Grid item>
-          <Paper style={{height:75, width:50,}}></Paper>
-        </Grid>
-        <Grid item>
-          <Paper style={{height:75, width:50,}}></Paper>
-        </Grid>
-        </Grid>
-        
-        <CheckBoxExample /> */}
-       {/* <ButtonGroup>
-         <Button variant='contained' color='primary' style={{fontSize: 25}} startIcon={<SaveIcon />}>Save As</Button>
-          <Button variant='contained' color='secondary' style={{fontSize: 25}} startIcon={<DeleteIcon />}>Discard</Button>
-        </ButtonGroup>*/}
-        <Typography variant="h10" component="div" >
-        <TableContainer>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>BLOODBANK</TableCell>
-            <TableCell align="right">PATIENT NO</TableCell>
-            <TableCell align="right">TIME</TableCell>
-             <TableCell align="right">BLOOD GROUP</TableCell> 
-            <TableCell align="right">DATE</TableCell>
-            <TableCell align="right"> OPTIONS</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.number}>
-              <TableCell>{row.bloodbank}</TableCell>
-              <TableCell align="right">{row.patientNO}</TableCell>
-              <TableCell align="right">{row.TIME}</TableCell>
-              <TableCell align="right">{row.bloodgroup}</TableCell>
-              <TableCell align="right">{row.date}</TableCell>
-              <ButtonStyled align="right" style={{padding: 15}} onClick={deleteRow(row.number)}/>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-   
-    </Typography>
-      </header>
-    </div>
-    </Container>
-    </ThemeProvider>
-  );
-}
-
-
 
 export default App;
